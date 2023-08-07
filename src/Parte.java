fe62e3e73e38af7b13505272e0e237f3d6148b66
enriquezleandro <enriquezleandro@gmail.com>
2023-08-07 12:27:21 -0300

    esta mal

--- src/Parte.java
+++ src/Parte.java
@@ -1,4 +1,9 @@
 public class Parte {
+<<<<<<< HEAD
 
 
+=======
+    ssadasdsa
+>>>>>>> parent of 821fd5d (Update Parte.java)
 }
+EXhttps://twitter.com/wallstwolverine/status/1688512019857543172?s=20
